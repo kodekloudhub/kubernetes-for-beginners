0680e6eb4c323dae6884c250b7a9e6584dde54e2
vpalazhi <vijin.palazhi@kodekloud.com>
2020-07-10 08:17:06 -0400

    updated test scenario

---
+++ ubuntu/vagrant/setup-hosts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+IFNAME=$1
+ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
+sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
+
+# remove ubuntu-bionic entry
+mkdir /home/osboxes
+useradd osboxes -s /bin/bash -d /home/osboxes
+chown osboxes /home/osboxes
+swapoff -a
+curl -L terminal.kodekloud.com | bash
+su -c "curl -L terminal.kodekloud.com | bash" vagrant
+sed -e '/^.*ubuntu-bionic.*/d' -i /etc/hosts
+
+# Update /etc/hosts about other hosts
+cat >> /etc/hosts <<EOF
+192.168.56.2  master
+192.168.56.3  worker-1
+192.168.56.4  worker-2
+EOF
+echo 'export PS1="${HOSTNAME} $"' >> /root/.bashrc
+
