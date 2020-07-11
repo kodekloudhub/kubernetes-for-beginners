afdd41465f2714439835e7fe035acd622bd177e0
vpalazhi <32654249+vpalazhi@users.noreply.github.com>
2020-07-10 20:39:10 -0400

    Update setup-hosts.sh

--- ubuntu/vagrant/setup-hosts.sh
+++ ubuntu/vagrant/setup-hosts.sh
@@ -19,6 +19,11 @@ cat >> /etc/hosts <<EOF
 192.168.56.3  worker-1
 192.168.56.4  worker-2
 EOF
-echo 'export PS1="${cyan}\h ${red}$ #${clear_attributes}"' >> /root/.bash_profile
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+echo 'export PS1="${cyan}\h ${red}$ ${clear_attributes}"' >> /root/.bash_profile
 echo 'export PS1="${cyan}\h ${red}$ ${clear_attributes}"' >> /home/vagrant/.bash_profile
 echo 'sudo -i' >> /home/vagrant/.bash_profile
